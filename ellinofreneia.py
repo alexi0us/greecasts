b5f51233019a9e7821399c35bf3f019e5a6fbdbd
alexis.kousounadis <alexis.kousounadis@ntt.eu>
2013-09-19 14:56:04 +0100

    Ellinofreneia module

---
+++ ellinofreneia.py
@@ -0,0 +1,47 @@
+import os
+import datetime
+import sys
+import common
+#import shutil
+import logging
+
+
+class ellinofreneia_functions:
+    cmn = common.common_functions()
+    now = datetime.datetime.now()
+    base_url = cmn.config_section_map("ellinofreneia")['base_url']
+    base_directory = cmn.config_section_map("home")['base_dir']
+
+    def ellin_is_file_downloaded(self):
+        flag = False
+        complete_audio_file = "ellinofreneia"\
+         + '_' + self.now.strftime("%d%m%Y") + '.mp3'
+        rootdir = self.base_directory + "ellinofreneia"
+        for path, subFolders, files in os.walk(rootdir):
+            for file in files:
+                if file == complete_audio_file:
+                    logging.info('File with name %s already exists.', \
+                                  complete_audio_file)
+                    flag = True
+        return flag
+
+    def find_actual_download_url(self, html_lines):
+        download_page = 'empty'
+        for line in html_lines:
+            if self.now.strftime("%d/%m/%Y") in line or self.now.strftime("%d/%m") in line:
+                logging.debug(line)
+                logging.info('A podcast found for %s', \
+                             self.now.strftime("%d-%m-%Y"))
+                process_a = line.split('href=\"')
+                for link in process_a:
+                    if link.startswith("DefaultArthro"):
+                        process_c = link.split('\">')
+                        download_page = process_c[0]
+                        podtitle = process_c[1][:-10].decode('utf-8')
+        if download_page == 'empty':
+            logging.info("No podcast found for: %s", \
+                                             self.now.strftime("%d/%m/%Y"))
+            logging.info("Exiting")
+            sys.exit()
+        else:
+            return self.base_url + '/' +download_page
